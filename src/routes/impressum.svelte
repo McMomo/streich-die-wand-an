2c82d97cf639090673b55db2bdfe292851746a62
Moritz Bantleon <moritz@bantleon.me>
2021-09-08 20:01:26 +0200

    Teampage and impressum

---
+++ src/routes/impressum.svelte
@@ -0,0 +1,48 @@
+<section class="shadow-md rounded-md overflow-hidden text-left m-8">
+	<div class="p-8">
+		<h2 class="text-xl font-semibold mb-2 text-kurz-green">Impressum</h2>
+		<p>David Kurz<br />
+		Malermeister Kurz<br />Jägerweg 21<br />89264 Weißenhorn</p>
+		<p>Telefon: 0173 8474759<br />
+		E-Mail: <a href="mailto:malermeisterkurz@outlook.de">malermeisterkurz@outlook.de</a><br />
+		</p>
+		<p><strong>Umsatzsteuer-ID: </strong><br />Umsatzsteuer-Identifikationsnummer nach §27a Umsatzsteuergesetz:<br />DE332690784</p>
+
+		<p><strong>Bildrechte:</strong><br />
+		Das Bild auf der Startseite ist aus der <a href='https://bilder.caparol.de/index.php/XTCsid/80d77005ab7e3abdca17fe1a1785b764' title='CAPAROL Bilddatenbank'>CAPAROL Bilddatenbank</a>
+		<br />
+		<div>Icons made by <a href='https://www.flaticon.com/authors/darius-dan' title='Darius Dan'>Darius Dan</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
+		<div>Icons made by <a href='https://www.flaticon.com/authors/pixelmeetup' title='Pixelmeetup'>Pixelmeetup</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
+		<div>Icons made by <a href='https://www.flaticon.com/authors/photo3idea-studio' title='photo3idea_studio'>photo3idea_studio</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
+		<div>Icons made by <a href='https://www.freepik.com' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
+		<div>Icons made by <a href='https://www.flaticon.com/authors/payungkead' title='Payungkead'>Payungkead</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
+
+		<br /><br /><h2>Disclaimer – rechtliche Hinweise</h2>
+
+		§ 1 Warnhinweis zu Inhalten<br />
+
+		Die kostenlosen und frei zugänglichen Inhalte dieser Webseite wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der bereitgestellten kostenlosen und frei zugänglichen journalistischen Ratgeber und Nachrichten. Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder. Allein durch den Aufruf der kostenlosen und frei zugänglichen Inhalte kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande, insoweit fehlt es am Rechtsbindungswillen des Anbieters.<br />
+
+		<br />
+
+		§ 2 Externe Links<br />
+
+		Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe Links unverzüglich gelöscht.<br />
+
+		<br />
+
+		§ 3 Urheber- und Leistungsschutzrechte<br />
+
+		Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht kommerziellen Gebrauch ist erlaubt.<br />
+
+		<br />
+
+		Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig.<br />
+
+		<br />
+
+		§ 4 Besondere Nutzungsbedingungen<br />
+
+		Soweit besondere Bedingungen für einzelne Nutzungen dieser Website von den vorgenannten Paragraphen abweichen, wird an entsprechender Stelle ausdrücklich darauf hingewiesen. In diesem Falle gelten im jeweiligen Einzelfall die besonderen Nutzungsbedingungen.<p>Quelle: <a href="https://www.juraforum.de/impressum-generator/">Artikel ansehen</a></p>
+	</div>
+</section>
---
+++ src/routes/team.svelte
@@ -0,0 +1,75 @@
+<script context="module">
+	import { fade } from 'svelte/transition'
+
+	export const prerender = true
+
+	const team = [
+		{ 
+			name: 'David Kurz',
+			position: 'Malermeister und Betriebsinhaber!',
+			image: '/assets/images/team/david.webp',
+			text: 'Bereits bei meinem Einstieg, in meine Ausbildung zum Maler und Lackierer, habe ich mich in meinen Job regelrecht verliebt. Die Möglichkeiten, die uns geboten werden, mit Farbe, Putz & kreativen Gestaltungstechniken aus einem üblichen Wohnraum ein wahres Herzstück zu zaubern und den verschiedenen Charakteren anzupassen, sind für mich bis heute faszinierend und immer wieder mit Freude anzusehen. Im Juli 2020 habe ich nach reiflicher Überlegung und gründlicher Vorbereitung den Schritt in die Selbständigkeit gewagt. Selbst & ständig mein Handwerk nach meinen eigenen Vorstellungen zu vertreten, gibt mir unglaublich viel Motivation, um auch Ihr Projekt perfekt zu betreuen und abzuwickeln. Ich habe mir vorgenommen unseren Kunden zu zeigen, wie man mit Kreativität unser Handwerk umsetzt. Mein Ziel ist es, unter anderem, jungen Menschen wieder die Vielfalt unseres Handwerks nahe zu bringen. Dieser Beruf ist mehr als nur eine Alternative. Wir sind Maler und Lackierer! Aus Überzeugung und Leidenschaft!'
+		},
+		{ 
+			name: 'Jürgen Seemüller',
+			position: 'Maler und Lackierer mit Leib & Seele!',
+			image: '/assets/images/team/juergen.webp',
+			text: 'Jürgen, der seit 36 Jahren mit Leib & Seele Maler und Lackierer ist, hat mich bestärkt und motiviert den Schritt in die Selbständigkeit zu tun. Aufgrund seines umfangreichen Wissens und jahrelanger Erfahrung hat er einen ausgezeichneten Ruf bei Bauträger und Privatkunden, die seine Arbeit äußerst schätzen. Durch seine jahrelange Erfahrung, unvergleichbare Motivation und detailverliebte Arbeitsweise passt er nicht nur optimal zu uns, sondern gibt uns das gewisse Etwas.'
+		},
+		{ 
+			name: 'Marvin Seemüller',
+			position: 'Maler und Lackierer, unser Frischling in 3. Generation.',
+			image: '/assets/images/team/marvin.webp',
+			text: 'Marvin ist der zweite Seemüller in unseren Reihen. Er war bereits in jungen Jahren Feuer & Flamme für diesen Beruf und tritt somit in die Fußstapfen von seinem Großvater und seinem Vater. Durch sein Engagement und seine Teamfähigkeit hat er sich bereits jetzt zu einer wichtigen Stütze in unserem kleinen Betrieb entwickelt. '
+		}
+	]
+
+	const partner = [
+		{
+			name: '',
+			url: ''
+		},
+	]
+</script>
+
+<svelte:head>
+	<title>Das sind wir!</title>
+</svelte:head>
+
+<section class="bg-kurz-white" transition:fade>
+	<div class="flex flex-wrap justify-center m-8">
+  		<div class="md:flex w-full rounded-xl shadow-md rounded-md overflow-hidden">
+    		<img src="/assets/images/team/team.webp" alt="Das Malermeister Kurz Team" class="rounded w-full max-h-80 align-middle border-none m-0 object-cover" />
+  		</div>
+	</div>
+	{#each team as member}
+		<figure class="md:flex bg-white rounded-xl m-8 p-8 md:p-0 shadow-md rounded-md overflow-hidden">
+		  <img class="w-32 h-32 md:w-64 md:h-auto md:rounded-none md:object-cover object-cover rounded-full mx-auto" src={member.image} alt={member.name} width="384" height="512">
+		  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+		    <blockquote>
+		    	{#if member.text.length > 500}
+					<p class="text-sm font-semibold">
+						“{member.text}”
+					</p>
+		      	{:else if member.text.length > 350}
+		      		<p class="text-md font-semibold">
+			        	“{member.text}”
+			      	</p>
+		    	{:else}
+			      <p class="text-lg font-semibold">
+			        “{member.text}”
+			      </p>
+		      	{/if}
+		    </blockquote>
+		    <figcaption class="font-medium">
+		      <div class="text-cyan-600">
+		        {member.name}
+		      </div>
+		      <div class="text-gray-500">
+		        {member.position}
+		      </div>
+		    </figcaption>
+		  </div>
+		</figure>
+	{/each}
+</section>
